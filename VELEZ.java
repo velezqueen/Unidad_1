c6c935bfca125b96d4682a53165e78e77f9a6d62
velezqueen <87016010+velezqueen@users.noreply.github.com>
2021-07-06 13:36:01 +0200

    Eclip1

---
+++ VELEZ.java
@@ -0,0 +1,12 @@
+package eclip1;
+
+public class VELEZ {
+
+	public static void main(String[] args) {
+		//System.out.println("hola");
+		System.out.println ("adios");
+		System.out.println ("FIN DEL PROGRAMA");
+
+	}
+
+}
